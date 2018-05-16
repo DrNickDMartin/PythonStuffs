e78779e20c1b7aeded5e20b806a99359b0395daf
Nick Martin <nick.d.martin@northumbria.ac.uk>
2018-05-16 12:47:06 +0100

    getting pygmsh working

--- gmshahmed.py
+++ gmshahmed.py
@@ -1,1 +1,11 @@
-print("Hellow world")
+import pygmsh as pg
+import meshio
+
+if __name__ == "__main__":
+    geom = pg.opencascade.Geometry(
+        characteristic_length_min=0.1,
+        characteristic_length_max=0.1)
+
+    geom.add_box([0,0,0],[1,1,1])
+    points, cells, point_data, cell_data, fd = pg.generate_mesh(geom)
+    meshio.write('test.vtu', points, cells, cell_data=cell_data)
